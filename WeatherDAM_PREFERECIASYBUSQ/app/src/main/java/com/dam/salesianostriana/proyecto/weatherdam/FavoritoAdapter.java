1ca4b149bf8c80cd8215b0f3b814efa734735056
jesuspa17 <jpallares94.jp@gmail.com>
2015-11-12 14:29:12 +0100

    casi completo

---
+++ WeatherDAM_PREFERECIASYBUSQ/app/src/main/java/com/dam/salesianostriana/proyecto/weatherdam/FavoritoAdapter.java
@@ -0,0 +1,77 @@
+package com.dam.salesianostriana.proyecto.weatherdam;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FavoritoAdapter extends RecyclerView.Adapter<FavoritoAdapter.ViewHolder>{
+
+    //private String[] mDataset;
+    private ArrayList<ItemFavorito> mDataset;
+    Context contexto;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView nombre;
+        public ImageButton btn_ver;
+
+        public ViewHolder(View v) {
+            super(v);
+            nombre = (TextView)v.findViewById(R.id.txtNombreCiudad);
+            btn_ver = (ImageButton) v.findViewById(R.id.imgLanzar);
+
+        }
+    }
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public FavoritoAdapter(ArrayList<ItemFavorito> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public FavoritoAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        // create a new view
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.recycler_item_favorito, viewGroup, false);
+
+        contexto = v.getContext();
+
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+
+    @Override
+    public void onBindViewHolder(final FavoritoAdapter.ViewHolder holder, final int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.nombre.setText(mDataset.get(position).getNombre());
+
+        holder.btn_ver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(contexto, MainActivity.class);
+                i.putExtra("titulo",Utils.listadoCiudadesFav.get(position));
+                contexto.startActivity(i);
+                ((Activity)contexto).finish();
+            }
+        });
+    }
+
+
+
+    @Override
+    public int getItemCount()  {
+        return mDataset.size();
+    }
+}
---
+++ WeatherDAM_PREFERECIASYBUSQ/app/src/main/java/com/dam/salesianostriana/proyecto/weatherdam/ItemFavorito.java
@@ -0,0 +1,21 @@
+package com.dam.salesianostriana.proyecto.weatherdam;
+
+/**
+ * Created by Jesús Pallares on 11/11/2015.
+ */
+public class ItemFavorito {
+
+    private String nombre;
+
+    public ItemFavorito(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
